71a980dedb10b4c1538c2e59a6f3cb6a5cb5f1bf
sayal35 <88419154+sayal35@users.noreply.github.com>
2023-01-19 15:04:53 +0545

    Update Word Assistant.c

--- Word Assistant.c
+++ Word Assistant.c
@@ -1,136 +1,92 @@
-#include <iostream>
-#include <time.h>
-#include <string>
-#include <stdlib.h>
-using namespace std;
 
-int main() {
-    int n,m;
-    cin>>n>>m;
-    srand(time(NULL));//  without this rand() function might continuously give the same value
-	int N = 10;
-  
-    // Function Call
-    randomPasswordGeneration(N);
-    
-    while(n--){
-        int stringLen = (rand() % m) +1; // getting random length
-        string s=""; // taking null string
-        for(int i=0; i<stringLen; i++){
-            if(rand() % 2 == 0 ){ // capital or small letter
-                s += 'A' + (rand() % 26);
-            }else{
-                s += 'a' + (rand() % 26);
-            }
-        }
-        cout<<s<<" ";
-    }
-	while(!done)
-	{
-		std::cin >> input;
-		if(input == '1')
-		{
-			sentence += temp + ' ';
-			word = "";
-			std::cout << sentence << std::endl;
-		}
-		else if(input == '2')
-		{
-			sentence += word + ' ';
-			word = "";
-			std::cout << sentence << std::endl;
-		}
-		else if(input != '0')
-		{
-			word += input;
-			for(it = dict.begin(); it != dict.end(); ++it)
-			{
-				if(word == (*it).substr(0, word.length()))
-				{
-					std::cout << (*it) << std::endl;
-					temp = (*it);
-					break;
-				}
-			}
-		}
-		else
-			done = true;
-	}
-    struct node{
-	int size ,end;
-	long long int maxfreq;
-	node* next[26];
-	node(){
-		size = 0,end = 0, maxfreq=0;
-		for(int i=0;i<26;i++)
-			next[i] = nullptr;
-	}
-  };
-	
-}
-void randomPasswordGeneration(int N)
+//BST 
+struct node
 {
-    // Initialize counter
-    int i = 0;
-  
-    int randomizer = 0;
-  
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-    srand((unsigned int)(time(NULL)));
-  
-    
-    
-  
-    // Array of small alphabets
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-  
-    // Array of capital alphabets
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-  
-  
-    // Stores the random password
-    char password[N];
-  
-    // To select the randomizer
-    // inside the loop
-    randomizer = rand() % 4;
-  
-    // Iterate over the range [0, N]
-    for (i = 0; i < N; i++) {
-  
-        
-         
-        
-         if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
+    int info;
+    struct node *left;								//declaring a node for left side insertion
+    struct node *right;								//declaring a node for right side insertion
+}*rootbs;
+
+//Class Declaration
+ 
+class BST
+{
+    public:
+        void insert(node*,node*);					//method to insert node
+        void display(node *, int);					//method to display node
+        BST()
+        {
+            rootbs = NULL;							//initializing to NULL
         }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
+};
+
+//Inserting Element into the Tree
+ 
+void BST::insert(node *tree, node *newnode)
+{
+    if (rootbs == NULL)								//if the tree is empty
+    {
+        rootbs = new node;							//creating new node
+        rootbs->info = newnode->info;				//store the value in the data
+        rootbs->left = NULL;						//making the left value null
+        rootbs->right = NULL;						//making the right value null
+        cout<<"Root Node is Added"<<endl;
+        return;
+    }
+    if (tree->info == newnode->info)				//if input value already in the tree
+    {
+        cout<<"Element already in the tree"<<endl;
+        return;
+    }
+    if (tree->info > newnode->info)					//if input is lesser than the tree value
+    {
+        if (tree->left != NULL)						//if the left of the tree value is NULL
+        {
+            insert(tree->left, newnode);			//perform insert operation on the left node
+        }
+        else
+        {
+            tree->left = newnode;					//create a new node on the left
+            (tree->left)->left = NULL;				//make the left of the left of the tree value NULL
+            (tree->left)->right = NULL;				//make the right of the left of the tree value NULL
+            cout<<"Node Added To Left"<<endl;
+            return;
+        }
+    }
+    else
+    {
+        if (tree->right != NULL)					//if the right of the tree value is NULL
+        {
+            insert(tree->right, newnode);			//perform insert operation on the right node
+        }
+        else
+        {
+            tree->right = newnode;					//create a new node on the left
+            (tree->right)->left = NULL;				//make the left of the right of the tree value NULL
+            (tree->right)->right = NULL;			//make the right of the right of the tree value NULL
+            cout<<"Node Added To Right"<<endl;
+            return;
         }
     }
 }
 
-void insert(string x){
-  	// Starting from the root node
-	node *temp = root;
-	for(int i=0;i<x.size();i++){
- 		 //  Using numbers 0-25 to represent alphabets
-		int t = x[i] - 'a';
-  		// Making a new node,if it doesn't exist
-		if(temp->next[t]==nullptr)
-			temp->next[t]=new node();
-		temp = temp->next[t];
-		  //  Increasing size of sub-graph rooted at this node
-		temp->size++;
-	}
-	// Increasing the frequency of this node as the word ends here
-	temp->end++;
-	
-	insertWithFreq(x,temp->end);
-} 
+//Display Tree Structure
+ 
+void BST::display(node *ptr, int level)
+{
+    int i;
+    if (ptr != NULL)								//if the tree is not empty
+    {
+        display(ptr->right, level+1);				//perform the display operation for the right side value
+        cout<<endl;
+        if (ptr == rootbs)							//if the value is the root value
+            cout<<"Root->:  ";
+        else
+        {
+            for (i = 0;i < level;i++)
+                cout<<"       ";
+        }
+        cout<<ptr->info;							//print the value
+        display(ptr->left, level+1);				//perform the display operation for the left side value
+    }
+}
